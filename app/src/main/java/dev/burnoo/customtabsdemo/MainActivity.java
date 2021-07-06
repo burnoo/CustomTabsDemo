e5a70edc2032bcc8e5f9a85c97159218e3b40fc0
burnoo <bruno.wieczorek@gmail.com>
2021-07-06 13:16:10 +0200

    Initial commit

---
+++ app/src/main/java/dev/burnoo/customtabsdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package dev.burnoo.customtabsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener((view) -> {
+            String url = "https://burno.tk/customtabs";
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(this, Uri.parse(url));
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String parameterValue = uri.getQueryParameter("parameter");
+            Toast.makeText(
+                    this,
+                    "Logged in from browser, parameter: " + parameterValue,
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+}
